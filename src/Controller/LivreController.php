773cccce18cb81bf3bcc3778c1da723426208b21
bu7ch <dutchr@live.fr>
2026-03-11 16:12:24 +0100

    fix render method

--- src/Controller/LivreController.php
+++ src/Controller/LivreController.php
@@ -14,5 +14,13 @@ class LivreController {
     public function index() {
         $livres = $this->repository->findAll();
         $this->render("livres/index", ['livres' => $livres]);
+    
     }
+
+    private function render(string $view, array $data = [], int $statusCode = 200){
+        http_response_code($statusCode);
+        extract($data);
+        require_once __DIR__ . '/../View' . $view .'.php';
+    }
+    // TODO: add show() / create()
 }
